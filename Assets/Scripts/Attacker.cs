1ca686ff5d22d5c25ca76c22841cee743515aedd
Hunter Morabito <hunter.morabito@gmail.com>
2020-06-11 19:43:36 -0400

    Adding Animation Events

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -7,18 +7,16 @@ public class Attacker : MonoBehaviour
     // Clamps move speed
     // Gives a slider in Unity!
     [Range(0f, 5f)]
-    [SerializeField]
-    private float _moveSpeed = 4f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float _currentSpeed = 0f;
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        transform.Translate(Vector2.left * Time.deltaTime * _moveSpeed);
+        transform.Translate(Vector2.left * Time.deltaTime * _currentSpeed);
+    }
+
+    public void SetMovementSpeed(float speed)
+    {
+        _currentSpeed = speed;
     }
 }
